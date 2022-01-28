fd83b88bb3659ca236287dfa2525f1f6377a0c95
mauricio-alves <mauricioalvesnutri@gmail.com>
2022-01-28 01:29:24 -0300

    Aula 02

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "958946ee31b7fa6a"; }
+/******/ 		__webpack_require__.h = function() { return "36362be868861a1a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,7 @@
 import { Box, Button, Text, TextField, Image } from "@skynexui/components";
+import React from "react";
+import { useRouter } from "next/router";
 import appConfig from "../config.json";
-
-function GlobalStyle() {
-  return (
-    <style global jsx>{`
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-      }
-      body {
-        font-family: "Open Sans", sans-serif;
-      }
-      /* App fit Height */
-      html,
-      body,
-      #__next {
-        min-height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-      #__next {
-        flex: 1;
-      }
-      #__next > * {
-        flex: 1;
-      }
-      /* ./App fit Height */
-    `}</style>
-  );
-}
 
 function Title(props) {
   const Tag = props.tag;
@@ -62,11 +33,12 @@ function Title(props) {
 // export default HomePage
 
 export default function PaginaInicial() {
-  const username = "mauricio-alves";
+  // const username = 'mauricio-alves';
+  const [username, setUsername] = React.useState("mauricio-alves");
+  const roteamento = useRouter();
 
   return (
     <>
-      <GlobalStyle />
       <Box
         styleSheet={{
           display: "flex",
@@ -101,6 +73,12 @@ export default function PaginaInicial() {
           {/* Formulário */}
           <Box
             as="form"
+            onSubmit={function (infosDoEvento) {
+              infosDoEvento.preventDefault();
+              console.log("Alguém submeteu o form");
+              roteamento.push("/chat");
+              // window.location.href = '/chat';
+            }}
             styleSheet={{
               display: "flex",
               flexDirection: "column",
@@ -121,8 +99,30 @@ export default function PaginaInicial() {
             >
               {appConfig.name}
             </Text>
-
+            /*{" "}
+            {/* <input
+              type="text"
+              value={username}
+              onChange={function (event) {
+                console.log("usuario digitou", event.target.value);
+                // Onde ta o valor?
+                const valor = event.target.value;
+                // Trocar o valor da variavel
+                // através do React e avise quem precisa
+                setUsername(valor);
+              }}
+            />{" "}
+            */}
             <TextField
+              value={username}
+              onChange={function (event) {
+                console.log("usuario digitou", event.target.value);
+                // Onde ta o valor?
+                const valor = event.target.value;
+                // Trocar o valor da variavel
+                // através do React e avise quem precisa
+                setUsername(valor);
+              }}
               fullWidth
               textFieldColors={{
                 neutral: {
